2b2ec558c2ea60858288f9b711883b9c79e37733
Imon Roy <royimonroy@gmail.com>
2021-07-27 22:52:43 +0530

    Revert "Updated to fix bugs"
    This reverts commit 0b7c0ead017ec74d947ac8e6a1eeb342c78f67b7.

--- Compiler/Compiler/settings.py
+++ Compiler/Compiler/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pycompiler',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,8 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'Compiler.urls'
@@ -118,9 +115,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-ALLOWED_HOSTS = ['*']
-
-CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
